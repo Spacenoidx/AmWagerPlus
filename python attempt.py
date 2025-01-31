72f717a90b1dc2acbb43d243a0aaaa6adc2e96aa
K.B. <k.mensah94@outlook.com>
2025-01-31 07:29:05 -0500

    first attempt

---
+++ python attempt.py
@@ -0,0 +1,11 @@
+url = "https://wagerapi.amwager.com/api/ToteProgram/GetAllEvents"
+import requests
+headers = {
+    'Content-Type': 'application/json',
+    'token': '93d73d6d-8fe3-4bb0-ba63-23cf6ef464f3',}
+
+response = requests.get(url, headers=headers)
+# print(response.json())
+
+for i in response.json()['ToteResponse']['Events']:
+    print(i)
